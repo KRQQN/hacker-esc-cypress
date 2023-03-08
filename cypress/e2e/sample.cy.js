e82ae09a774f483572019034756ce5c89dd77b9a
Rkron91 <robin.kron@lerniastudent.se>
2023-03-08 15:48:10 +0100

    timer added

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -5,9 +5,9 @@ describe("site is running on localhost", () => {
     });
 
     it("On-site button have css & leads to challenges.html", () => {
-        cy.visit("http://localhost:8080");
-        cy.get(".hero-cta > .button secondary").should("have.css");
-        cy.get(".hero-cta > .button secondary").click();
-        cy.url().contains("challenges.html");
+        cy.visit("http://localhost:8080")
+        cy.wait(2000)
+        cy.get(".hero-cta > .button secondary").should("have.css")
+        cy.get(".hero-cta > .button secondary").click().url().contains("challenges.html")
     });
 })
