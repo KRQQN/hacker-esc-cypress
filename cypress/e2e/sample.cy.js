7e0406365d2bed498911f57b418a28b9ff78206c
Rkron91 <robin.kron@lerniastudent.se>
2023-03-08 16:37:11 +0100

    fixed issue

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -1,13 +1,12 @@
 describe("site is running on localhost", () => {
+  it("connects to loacalhost landing page", () => {
+    cy.visit("http://localhost:8080");
+  });
 
-    it("connects to loacalhost landing page", () => {
-        cy.visit("http://localhost:8080");
-    });
-
-    it("On-site button have css & leads to challenges.html", () => {
-        cy.visit("http://localhost:8080")
-        cy.wait(2000)
-        cy.get(".hero-cta > .button secondary").should("have.css")
-        cy.get(".hero-cta > .button secondary").click().url().contains("challenges.html")
-    });
-})
+  it("On-site button leads to challenges.html", () => {
+    cy.visit("http://localhost:8080");
+    cy.wait(1000);
+    cy.get("div.hero-cta > a.button.secondary").click();
+    cy.url().should("include", "challenges.html");
+  });
+});
